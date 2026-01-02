db5eb1586be7f113bdf36bca6a36938c20c8b75e
Emrullah Günay <info@kobinerji.com>
2026-01-02 08:37:50 +0300

    fix: MONTHS sabiti eklendi

--- src/BordroTakip.tsx
+++ src/BordroTakip.tsx
@@ -29,6 +29,9 @@ import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import * as XLSX from 'xlsx';
 
+// --- SABİTLER ---
+const MONTHS = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'];
+
 // --- TİP TANIMLAMALARI ---
 
 type DayType = 'Normal' | 'Pazar' | 'Resmi Tatil' | 'Raporlu' | 'İzinli';
